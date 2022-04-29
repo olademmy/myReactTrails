1cd690d8a04ffa3f9c6a591011cc7abb8c83f5ac
Elemo Olutola <tb3d85@gmail.com>
2022-04-29 15:47:07 +0100

    Assignment 4 Modify addExpense form

--- Components/NewExpense/NewExpense.js
+++ Components/NewExpense/NewExpense.js
@@ -1,13 +1,24 @@
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 function NewExpense(props) {
+  const [editable, seteditable] = useState(false);
+  const onClickHandler = () => {
+    seteditable(true);
+  };
+  const onCancelHandler = () => {
+    seteditable(false);
+  };
   const saveDataHandler = (expensedata) => {
     const expenseData = { ...expensedata, id: Math.random.toString() };
     props.onAddData(expenseData);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveData={saveDataHandler} />
+      {!editable && <button onClick={onClickHandler}>Add Expense</button>}
+      {editable && (
+        <ExpenseForm onSaveData={saveDataHandler} onCancel={onCancelHandler} />
+      )}
     </div>
   );
 }
